a820f7f18668c59b8b925185fe4572bea1f72841
aarnav <aarnav@srlabs.de>
2026-01-07 16:15:27 +0100

    llvm-ar

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 clang++ -std=c++17 -c asd.cc -o thrower.o
+llvm-ar rcs libthrower.a thrower.o
 cargo build --release
 clang++ -std=c++17 ./main.cc ./target/release/libasdf.a -o ./release
 
 clang++ -std=c++17 -c asd.cc -o thrower.o
+llvm-ar rcs libthrower.a thrower.o
 cargo build
 clang++ -std=c++17 ./main.cc ./target/debug/libasdf.a -o ./debug
